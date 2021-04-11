08465fed5c6b22d2427d1bc7058ab5096ea0db44
Sarah McNelis <G00398343@gmit.ie>
2021-04-11 17:17:27 +0100

    pands-problem-sheet

--- weeklytask05-weekday.py
+++ weeklytask05-weekday.py
@@ -6,6 +6,7 @@ weekend = ["Saturday", "Sunday"]
 import datetime # importing this module in order to access current date/time as an object
 x = datetime.datetime.now() # this will give current date/time as per my pc's clock
 today = x.strftime("%A")
+#print(today)
 # strftime converts into a string
 # %A access the actual word of which day it is
 
@@ -13,6 +14,6 @@ if today == 'Saturday' or 'Sunday':
     print("It is the weekend, yay!")
 else:
     print("Yes, unfortunately today is a weekday.")
-#using and if and else statements here. Once determined above what day is it
-# if it's not the weekend it will print from the if statement
+# using an if and else statements here. Once determined above what day is it
+# if it is the weekend it will print from the if statement
 # when the if statment is false it will run the else statment
